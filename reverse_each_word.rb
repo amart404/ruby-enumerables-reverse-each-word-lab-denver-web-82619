ca37180fb57602ac57aaffeb6d69ac68ca94a7b8
Aaron Martinez <aaronmarti17@gmail.com>
2019-08-22 23:27:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word_collect(string)
   original_array = string.split(" ")
   new_array = []
-  array.collect do|string|
+  original_array.collect do|string|
     new_array << string.reverse
   end
   new_array.join(" ")
